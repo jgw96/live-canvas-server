465a1e094c6dcb73f0a2b73fa4f71b98d6fc7dc0
Justin Willis <jgw9617@gmail.com>
2022-01-22 17:24:55 -0800

    feat(): update socket.io

--- dist/server.js
+++ dist/server.js
@@ -3,27 +3,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const socket_io_1 = require("socket.io");
+const http_1 = require("http");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(cors());
-let http = require("http").Server(app);
-let io = require("socket.io")(http);
 // simple '/' endpoint sending a Hello World
 // response
 app.get("/", (req, res) => {
     res.send("hello world");
 });
+const httpServer = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(httpServer, {
+    cors: {
+        origin: "http://localhost:8001",
+        methods: ["GET", "POST"],
+    },
+});
 io.on("connection", (socket) => {
     console.log("a user connected");
-    const room = socket.handshake['query']['r_var'];
+    const room = socket.handshake["query"]["r_var"];
     socket.join(room);
-    socket.on('drawing', (data) => {
-        socket.broadcast.to(room).emit('drawing', data);
+    socket.on("drawing", (data) => {
+        socket.broadcast.to(room).emit("drawing", data);
     });
     // start our simple server up on localhost:3000
 });
-const server = http.listen(process.env.PORT || 3000, () => {
+httpServer.listen(process.env.PORT || 3000, () => {
     console.log("listening on *:3000");
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import * as cors from 'cors';
+import * as cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
@@ -8,28 +10,33 @@ app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(cors());
 
-let http = require("http").Server(app);
-let io = require("socket.io")(http);
-
 // simple '/' endpoint sending a Hello World
 // response
 app.get("/", (req: any, res: any) => {
   res.send("hello world");
 });
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:8001",
+    methods: ["GET", "POST"],
+  },
+});
+
 io.on("connection", (socket: any) => {
   console.log("a user connected");
 
-  const room = socket.handshake['query']['r_var'];
+  const room = socket.handshake["query"]["r_var"];
   socket.join(room);
 
-  socket.on('drawing', (data: any) => {
-    socket.broadcast.to(room).emit('drawing', data)
+  socket.on("drawing", (data: any) => {
+    socket.broadcast.to(room).emit("drawing", data);
   });
 
   // start our simple server up on localhost:3000
 });
 
-const server = http.listen(process.env.PORT || 3000, () => {
+httpServer.listen(process.env.PORT || 3000, () => {
   console.log("listening on *:3000");
-})
+});
